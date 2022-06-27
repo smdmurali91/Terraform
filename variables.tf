aec14effa1c7e8305a87e7735c51e7ab5beb8ddc
smdmurali91 <108254028+smdmurali91@users.noreply.github.com>
2022-06-27 11:51:10 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,9 +1,3 @@
-variable "aws_region" {
-    type = string
-    description = "AWS Region to use for resources"
-    default = "us-east-1"
-}
-
 variable "instance_type" {
     type = string
     description = "Type for EC2 instance"
